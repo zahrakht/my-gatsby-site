e2bed3d30c3ec12bb9b5509ba7debf4fb9b0adb2
zahra.kh.t <zahra.kh.t@gmail.com>
2021-11-21 21:53:03 +0330

    Finished Gatsby Tutorial Part 4

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import * as React from 'react'
 import Layout from '../components/layout'
 import { StaticImage } from 'gatsby-plugin-image'
 
+
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
